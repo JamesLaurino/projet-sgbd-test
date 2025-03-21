0b39aab29e1d0186292b2c7954062bc453f6881c
jamesLaurino <petersthomas90@gmail.com>
2025-03-21 21:38:08 +0100

    views exercices optimisation du tableau de produits

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -4,34 +4,27 @@ namespace App\Http\Controllers;
 
 class ProduitController extends Controller
 {
+    public $produits = [
+        ['id' => 1, 'nom' => 'PC Portable', 'prix' => 1200, 'en_promo' => true],
+        ['id' => 2, 'nom' => 'Souris Gaming', 'prix' => 80, 'en_promo' => false],
+        ['id' => 3, 'nom' => 'Clavier Mécanique', 'prix' => 150, 'en_promo' => true],
+        ['id' => 4, 'nom' => 'Casque Audio', 'prix' => 100, 'en_promo' => false],
+    ];
+
+
     public function index()
     {
-        $produits = [
-            ['id' => 1, 'nom' => 'PC Portable', 'prix' => 1200, 'en_promo' => true],
-            ['id' => 2, 'nom' => 'Souris Gaming', 'prix' => 80, 'en_promo' => false],
-            ['id' => 3, 'nom' => 'Clavier Mécanique', 'prix' => 150, 'en_promo' => true],
-            ['id' => 4, 'nom' => 'Casque Audio', 'prix' => 100, 'en_promo' => false],
-        ];
-        return view('produit',['produits' => $produits]);
+        return view('produit',['produits' => $this->produits]);
     }
 
     public function getById($id)
     {
-        $produits = [
-            ['id' => 1, 'nom' => 'PC Portable', 'prix' => 1200, 'en_promo' => true],
-            ['id' => 2, 'nom' => 'Souris Gaming', 'prix' => 80, 'en_promo' => false],
-            ['id' => 3, 'nom' => 'Clavier Mécanique', 'prix' => 150, 'en_promo' => true],
-            ['id' => 4, 'nom' => 'Casque Audio', 'prix' => 100, 'en_promo' => false],
-        ];
-
         $res = [];
-
-        foreach ($produits as $produit) {
+        foreach ($this->produits as $produit) {
             if($produit['id'] == $id){
                 $res = $produit;
             }
         }
-
         return view('product-detail',['res' => $res]);
     }
 }
