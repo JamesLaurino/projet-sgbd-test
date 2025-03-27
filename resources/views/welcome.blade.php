c4ae397b165a117db70b75836269297fdbeef1d6
jamesLaurino <petersthomas90@gmail.com>
2025-03-27 12:21:40 +0100

    blade template using other directive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,14 @@
 @endsection
 
 @section("body")
-    <div class="container-fluid d-flex justify-content-center bg-dark">
-        <h1 class="m-3 text-white">Hello from Projet</h1>
-    </div>
 
-    <div class="container-fluid d-flex justify-content-center mt-5">
+    @php
+        $demo = true;
+        echo $demo;
+    @endphp
+
+    <div @class(["container-fluid", "d-flex", "justify-content-center", "mt-5", "text-danger" => $demo])>
         <p>Hello world with bootstrap</p>
     </div>
+
 @endsection
