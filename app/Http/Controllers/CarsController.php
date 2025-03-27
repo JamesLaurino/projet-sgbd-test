486ae9f8bff5c3990556c6bfe895694aff60251a
jamesLaurino <petersthomas90@gmail.com>
2025-03-27 11:36:52 +0100

    refactoring exercice-routing controller & view

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -7,6 +7,6 @@ class CarsController extends Controller
     public function index()
     {
         $cars = array("Volvo", "BMW", "Mercedes");
-        return view('cars', ['cars' => $cars]);
+        return view('exercice-routing.cars', ['cars' => $cars]);
     }
 }
